25a066dbf127b3a403e17e588b24293947811e5f
unknown <l3szcz@gmail.com>
2018-06-23 15:03:20 +0200

    Hash user password before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const { SHA256 } = require("crypto-js");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-var data = {
-  id: 10
-};
+var password = "123abc!";
 
-var token = jwt.sign(data, "123abc");
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, "123abc");
-console.log("DECODED", decoded);
+var hashedPassword =
+  "$2a$10$MyWPbWXAmnn0Xhw16ClqiOZm5T4NMrmUzTqHhXVoQOfjeNR1A4op2";
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id: 10
+// };
+
+// var token = jwt.sign(data, "123abc");
+// console.log(token);
+
+// var decoded = jwt.verify(token, "123abc");
+// console.log("DECODED", decoded);
 
 // var message = "Kamil Doroszewicz";
 // var hash = SHA256(message).toString();
